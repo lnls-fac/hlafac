4dc1ed285821cbe0a21f7eb223ecfc958b46ac0f
afonsoharuo <afonso.mukai@lnls.br>
2014-02-24 10:56:39 -0300

    Add setup and .gitignore for utils package

---
+++ utilities/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(name='Utils',
+      version='1.0',
+      description='Utilities',
+      author='Afonso Haruo Carnielli Mukai',
+      author_email='afonso.mukai@lnls.br',
+      url='https://github.com/lnls-fac/sirius_hla',
+      packages=['utils'],
+     )
