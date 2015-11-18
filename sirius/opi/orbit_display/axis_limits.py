7fea38c3cb4fdff45522523ba2b92349a18a3be6
luana <luana.vilela@lnls.br>
2015-11-18 16:46:03 -0200

    Add initial version of orbit display

---
+++ sirius/opi/orbit_display/axis_limits.py
@@ -0,0 +1,16 @@
+from org.csstudio.opibuilder.scriptUtil import PVUtil
+import math
+
+y = PVUtil.getDoubleArray(pvArray[0])
+y = [math.fabs(i) for i in y]
+
+default = 10
+maximum = max(y)
+
+if maximum < default:
+	widget.setPropertyValue("axis_1_maximum",  default)
+	widget.setPropertyValue("axis_1_minimum", -default)
+else:
+	widget.setPropertyValue("axis_1_maximum",  maximum)
+	widget.setPropertyValue("axis_1_minimum", -maximum)
+	
