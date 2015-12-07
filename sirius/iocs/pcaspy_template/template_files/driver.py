ba2db78110dcb8beb2aed956fa670064f5dd063e
Afonso <afonso.mukai@lnls.br>
2015-12-07 09:36:08 -0200

    Add initial version of pcaspy soft IOC template

---
+++ sirius/iocs/pcaspy_template/template_files/driver.py
@@ -0,0 +1,14 @@
+
+import pcaspy
+
+
+class PCASDriver(pcaspy.Driver):
+    
+    def __init__(self):
+        super().__init__()
+        
+    # def read(self, reason):
+    #     pass
+         
+    # def write(self, reason, value):
+    #     pass
