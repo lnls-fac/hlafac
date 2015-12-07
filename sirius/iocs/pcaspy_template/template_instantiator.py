32c2f7c01aeaee4d2d1411965d21a06f023feae9
Afonso <afonso.mukai@lnls.br>
2015-12-07 10:14:03 -0200

    Simpify pcaspy_template directory structure

--- sirius/iocs/pcaspy_template/template_instantiator.py
+++ sirius/iocs/pcaspy_template/template_instantiator.py
@@ -9,31 +9,25 @@ TEMPLATE_DIR = 'template_files'
 class TemplateInstatiator:
     
     def __init__(self, args):
-        self.name = args.name
         if args.dest is not None:
-            self.destination = os.path.realpath(args.dest)
+            d = os.path.realpath(args.dest)
         else:
-            self.destination = os.path.realpath(os.curdir)
+            d = os.path.realpath(os.curdir)
+        self.destination = os.path.join(d, args.name)    
+        
         s = os.path.dirname(os.path.realpath(__file__))
         self.source = os.path.join(s, TEMPLATE_DIR)
     
     def instantiate(self):
-        self._check_destination_empty()
-        self._create_directories()
+        self._create_directory()
         self._copy_files()
     
-    def _check_destination_empty(self):
-        contents = os.listdir(self.destination)
-        if len(contents) > 0:
-            raise Exception('destination directory not empty')
-    
-    def _create_directories(self):
-        os.mkdir(os.path.join(self.destination, self.name))
-        os.mkdir(os.path.join(self.destination, 'test'))
+    def _create_directory(self):
+        # Will fail if directory already exists
+        os.mkdir(self.destination)
     
     def _copy_files(self):
         contents = os.listdir(self.source)
         filenames = [os.path.join(self.source, fn) for fn in contents]
-        file_destination = os.path.join(self.destination, self.name)
         for fn in filenames:
-            shutil.copy(fn, file_destination)
+            shutil.copy(fn, self.destination)
