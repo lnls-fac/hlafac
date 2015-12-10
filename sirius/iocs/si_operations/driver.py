b66b8063a6460838bb2bfe2d70036b47f493804f
Afonso <afonso.mukai@lnls.br>
2015-12-10 10:36:36 -0200

    Refactor si_operations

--- sirius/iocs/si_operations/driver.py
+++ sirius/iocs/si_operations/driver.py
@@ -4,18 +4,19 @@ import pvs
 
 
 class PCASDriver(pcaspy.Driver):
-    
+
     def __init__(self):
         super().__init__()
         self._initialise_pvs()
-        
+
     def read(self, reason):
         return super().read(reason)
-         
+
     def write(self, reason, value):
         return super().write(reason, value)
 
     def _initialise_pvs(self):
-        self.setParam('SHIFT-TYPE', 0)
-        self.setParam('MESSAGE', pvs.pvdb['MESSAGE']['value'])
+        for pv in pvs.pvdb.keys():
+            self.setParam(pv, pvs.pvdb[pv]['value'])
+
         self.updatePVs()
