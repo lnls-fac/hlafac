02c1080326d81aa81c3de6dced02efc473f86d1f
Isabella Stevani <isabella.stevani@lnls.br>
2016-07-27 13:20:37 -0300

    Add orbit correction error

--- sirius/iocs/si_orbit_correction/pvs.py
+++ sirius/iocs/si_orbit_correction/pvs.py
@@ -7,7 +7,7 @@ pvdb = {
     'SICO-SOFB-AVGORBIT-X': {'type': 'float', 'count': numBPM},
     'SICO-SOFB-AVGORBIT-Y': {'type': 'float', 'count': numBPM},
     'SICO-SOFB-AVGORBIT-NUMSAMPLES': {'type': 'int', 'value': 1},
-    'SICO-SOFB-ERROR': {'type': 'enum', 'enums': ['None', 'MeasRespmError', 'SetAvgOrbitNumSamplesError', 'ReadOrbitError', 'SetRespmError', 'SetRefOrbitError', 'CalcEstatDataError']},
+    'SICO-SOFB-ERROR': {'type': 'enum', 'enums': ['None', 'MeasRespmError', 'SetAvgOrbitNumSamplesError', 'ReadOrbitError', 'SetRespmError', 'SetRefOrbitError', 'CalcEstatDataError', 'CorOrbitError']},
     'SICO-SOFB-RESPM': {'type': 'string', 'value': 'respm_hv_f_va'},
     'SICO-SOFB-REFORBIT-X': {'type': 'string', 'value': 'orbit_x_null'},
     'SICO-SOFB-REFORBIT-Y': {'type': 'string', 'value': 'orbit_y_null'},
