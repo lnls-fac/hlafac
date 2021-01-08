b84ba18b50b4ed11750962161659981bad7e3e99
Fernando <fernandohds564@gmail.com>
2021-01-08 12:38:02 -0300

    SITRAJFIT.ENH: First working version.

--- scripts/sirius-hla-si-ap-trajfit.py
+++ scripts/sirius-hla-si-ap-trajfit.py
@@ -6,19 +6,14 @@ import sys
 import argparse as _argparse
 
 from siriushla.sirius_application import SiriusApplication
-from siriuspy.envars import VACA_PREFIX
 
-from siriushlafac.si_ap_trajfit import MainWindow
+from siriushlafac.si_ap_trajfit import SIFitTrajWindow
 
 
 parser = _argparse.ArgumentParser(
     description="Run Injection Trajectory Fitting Interface.")
-parser.add_argument(
-    '-p', "--prefix", type=str, default=VACA_PREFIX,
-    help="Define the prefix for the PVs in the window.")
-
 args = parser.parse_args()
 
 app = SiriusApplication()
-app.open_window(MainWindow, parent=None, prefix=args.prefix)
+app.open_window(SIFitTrajWindow, parent=None)
 sys.exit(app.exec_())
--- siriushlafac/si_ap_trajfit/main.py
+++ siriushlafac/si_ap_trajfit/main.py
@@ -1,167 +1,261 @@
 """Main module of the Application Interface."""
 
+import numpy as np
+import matplotlib.pyplot as mplt
+import matplotlib.gridspec as mgs
+from matplotlib import rcParams
+
 from qtpy.QtCore import Qt
-from qtpy.QtWidgets import QWidget, QDockWidget, QSizePolicy, QVBoxLayout, \
-    QPushButton, QHBoxLayout, QMenu, QMenuBar, QAction, QStatusBar
+from qtpy.QtGui import QDoubleValidator
+from qtpy.QtWidgets import QWidget, QPushButton, QGridLayout, QSpinBox, \
+    QDoubleSpinBox, QLabel, QGroupBox, QMainWindow, QLineEdit
+
 import qtawesome as qta
 
-from siriuspy.envars import VACA_PREFIX as LL_PREF
 from siriuspy.sofb.csdev import SOFBFactory
 from siriushla import util
 from siriushla.widgets import SiriusMainWindow
-from siriushla.widgets import PyDMLogLabel
-from siriushla.widgets.windows import create_window_from_widget
+from siriushla.widgets import MatplotlibWidget
 
-from apsuite.commissioning_scripts.inj_traj_fitting import
+from apsuite.commissioning_scripts.inj_traj_fitting import SIFitInjTraj
+from apsuite.optics_analysis import TuneCorr
+
+rcParams.update({
+    'font.size': 12, 'axes.grid': True, 'grid.linestyle': '--',
+    'grid.alpha': 0.5})
 
 
-class MainWindow(SiriusMainWindow):
-    def __init__(self, parent=None, prefix='', acc='SI'):
+class SIFitTrajWindow(QMainWindow):
+    """."""
+
+    def __init__(self, parent=None):
+        """."""
         super().__init__(parent=parent)
-        self.prefix = prefix + acc + '-Glob:AP-SOFB:'
-        self._csorb = SOFBFactory.create(acc)
+        self._csorb = SOFBFactory.create('SI')
+        self.fit_traj = SIFitInjTraj()
+        self.tunecorr = TuneCorr(
+            self.fit_traj.model, 'SI', method='Proportional',
+            grouping='TwoKnobs')
+
         self.setupui()
-        self.setObjectName(acc+'App')
+        self.setObjectName('SIApp')
         self.setWindowIcon(
-            qta.icon('fa5s.hammer', color=util.get_appropriate_color(acc)))
-
-    @property
-    def acc(self):
-        return self._csorb.acc
-
-    @property
-    def acc_idx(self):
-        return self._csorb.acc_idx
-
-    @property
-    def isring(self):
-        return self._csorb.isring
+            qta.icon('fa5s.hammer', color=util.get_appropriate_color('SI')))
 
     def setupui(self):
+        """."""
         self.setWindowModality(Qt.WindowModal)
-        self.setWindowTitle(self.acc + " - SOFB")
+        self.setWindowTitle("SI - Trajectory Fitting")
         self.setDocumentMode(False)
         self.setDockNestingEnabled(True)
-
-        self.ioc_log = self._create_log_docwidget()
-        self.orbit_regist = self._create_orbit_registers()
-        self.sofb_control = self._create_ioc_controllers()
-
-        self.addDockWidget(Qt.LeftDockWidgetArea, self.ioc_log)
-        self.addDockWidget(Qt.LeftDockWidgetArea, self.orbit_regist)
-        self.addDockWidget(Qt.RightDockWidgetArea, self.sofb_control)
 
         mwid = self._create_central_widget()
         self.setCentralWidget(mwid)
 
-        self._create_menus()
-
     def _create_central_widget(self):
-        ctrls = self.orb_regtr.get_registers_control()
-        chans, ctr = OrbitWidget.get_default_ctrls(self.prefix, self.acc)
-        self._channels = chans
-        ctrls.update(ctr)
-        return OrbitWidget(self, self.prefix, ctrls, self.acc)
+        wid = QWidget(self)
+        wid.setLayout(QGridLayout())
 
-    def _create_orbit_registers(self):
-        # Create Context Menus for Registers and
-        # assign them to the clicked signal
-        wid = QDockWidget(self)
-        wid.setWindowTitle("Orbit Registers")
-        sz_pol = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
-        sz_pol.setVerticalStretch(1)
-        wid.setSizePolicy(sz_pol)
-        wid.setFloating(False)
-        wid.setFeatures(QDockWidget.AllDockWidgetFeatures)
-        wid.setAllowedAreas(Qt.AllDockWidgetAreas)
-        wid.setObjectName('doc_OrbReg')
-        wid.setStyleSheet("#doc_OrbReg{min-width:20em; min-height:14em;}")
+        wid.layout().addWidget(
+            QLabel('<h1> SI - Fit Trajectory </h1>', wid), 0, 0, 1, 2,
+            alignment=Qt.AlignCenter)
 
-        wid_cont = OrbitRegisters(self, self.prefix, self.acc, 7)
-        wid.setWidget(wid_cont)
-        self.orb_regtr = wid_cont
+        fig_wid = self.make_figure(wid)
+        wid.layout().addWidget(fig_wid, 1, 0, 5, 1)
+
+        tune = self.get_tune_fit_widget(wid)
+        wid.layout().addWidget(tune, 1, 1)
+
+        ctrls = self.get_param_control_widget(wid)
+        wid.layout().addWidget(ctrls, 3, 1)
+
+        results = self.get_results_widget(wid)
+        wid.layout().addWidget(results, 5, 1)
+
+        wid.layout().setRowStretch(2, 2)
+        wid.layout().setRowStretch(4, 2)
         return wid
 
-    def _create_ioc_controllers(self):
-        docwid = QDockWidget(self)
-        docwid.setWindowTitle("IOC Control")
-        sz_pol = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
-        sz_pol.setVerticalStretch(1)
-        docwid.setSizePolicy(sz_pol)
-        docwid.setFloating(False)
-        docwid.setFeatures(QDockWidget.AllDockWidgetFeatures)
-        docwid.setAllowedAreas(Qt.AllDockWidgetAreas)
+    def make_figure(self, parent):
+        """."""
+        self.fig = mplt.figure(figsize=(7, 14))
+        fig_widget = MatplotlibWidget(self.fig, parent=parent)
 
-        ctrls = self.orb_regtr.get_registers_control()
-        wid = SOFBControl(self, self.prefix, ctrls, self.acc)
-        docwid.setWidget(wid)
-        return docwid
+        gs = mgs.GridSpec(3, 1)
+        gs.update(left=0.15, right=0.98, top=0.94, bottom=0.1, hspace=0.35)
+        self.axes_x = self.fig.add_subplot(gs[0, 0])
+        self.axes_y = self.fig.add_subplot(gs[1, 0], sharex=self.axes_x)
+        self.axes_s = self.fig.add_subplot(gs[2, 0], sharex=self.axes_y)
 
-    def _create_log_docwidget(self):
-        docwid = QDockWidget(self)
-        docwid.setWindowTitle('IOC Log')
-        sz_pol = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
-        docwid.setSizePolicy(sz_pol)
-        docwid.setFloating(False)
-        docwid.setObjectName('doc_IOCLog')
-        docwid.setStyleSheet("#doc_IOCLog{min-width:20em; min-height:34em;}")
-        wid_cont = QWidget()
-        docwid.setWidget(wid_cont)
-        vbl = QVBoxLayout(wid_cont)
-        vbl.setContentsMargins(0, 0, 0, 0)
-        pdm_log = PyDMLogLabel(wid_cont, init_channel=self.prefix+'Log-Mon')
-        pdm_log.setAlternatingRowColors(True)
-        pdm_log.maxCount = 2000
-        vbl.addWidget(pdm_log)
-        hbl = QHBoxLayout()
-        vbl.addLayout(hbl)
-        hbl.addStretch()
-        pbtn = QPushButton('Clear Log', wid_cont)
-        pbtn.clicked.connect(pdm_log.clear)
-        hbl.addWidget(pbtn)
-        hbl.addStretch()
-        return docwid
+        bpmpos = self.fit_traj.twiss.spos[self.fit_traj.bpm_idx]
+        zer = np.zeros(bpmpos.size)
+        self.line_measx = self.axes_x.plot(
+            bpmpos, zer, '-d', label='Trajectory')[0]
+        self.line_measy = self.axes_y.plot(
+            bpmpos, zer, '-d', label='Trajectory')[0]
+        self.line_meass = self.axes_s.plot(bpmpos, zer, '-k')[0]
+        self.line_fitx = self.axes_x.plot(
+            bpmpos, zer, '-o', label='Fitting', linewidth=1)[0]
+        self.line_fity = self.axes_y.plot(
+            bpmpos, zer, '-o', label='Fitting', linewidth=1)[0]
+        self.axes_x.set_title(
+            r"x = {:.3f}mm x' = {:.3f}mrad  $\delta$ = {:.2f}%".format(
+                0.0, 0.0, 0.0))
+        self.axes_y.set_title(
+            r"y = {:.3f}mm y' = {:.3f}mrad".format(0.0, 0.0))
+        self.axes_y.legend(
+            loc='upper center', bbox_to_anchor=(0.5, -0.05), fontsize='small',
+            ncol=2)
+        self.axes_s.set_xlabel('Position [m]')
+        self.axes_x.set_ylabel('X [mm]')
+        self.axes_y.set_ylabel('Y [mm]')
+        self.axes_s.set_ylabel('Sum [counts]')
+        [label.set_visible(False) for label in self.axes_x.get_xticklabels()]
+        [label.set_visible(False) for label in self.axes_y.get_xticklabels()]
+        return fig_widget
 
-    def _create_menus(self):
-        menubar = QMenuBar(self)
-        menubar.setNativeMenuBar(False)
+    def get_tune_fit_widget(self, parent):
+        """."""
+        wid = QGroupBox('Tune Adjustment', parent)
+        wid.setLayout(QGridLayout())
 
-        menuopen = QMenu('Open', menubar)
-        actions = (
-            ("&SOFB Control", "SOFB Control", '', True, self.sofb_control),
-            ("IOC &Log", "IOC Log", '', True, self.ioc_log),
-            ("&Registers", "Orbit Registers", '', True, self.orbit_regist))
-        self.setMenuBar(menubar)
-        for name, tool, short, check, doc in actions:
-            action = QAction(name, self)
-            action.setToolTip(tool)
-            action.setShortcut(short)
-            action.setCheckable(check)
-            action.setChecked(check)
-            action.setEnabled(True)
-            action.setVisible(True)
-            action.toggled.connect(doc.setVisible)
-            doc.visibilityChanged.connect(action.setChecked)
-            menuopen.addAction(action)
-        menubar.addAction(menuopen.menuAction())
+        self.wid_nux = QDoubleSpinBox(wid)
+        self.wid_nuy = QDoubleSpinBox(wid)
+        self.lab_tune = QLabel(wid)
+        pusb = QPushButton('Adjust Tune', wid)
+        pusb.clicked.connect(self._adjust_tune)
 
-        actbpm = QAction('Show BPM List', menubar)
-        Window = create_window_from_widget(SelectBPMs, title='BPM List')
-        util.connect_window(
-            actbpm, Window, self, bpm_list=self._csorb.bpm_names)
-        menubar.addAction(actbpm)
+        self.wid_nux.setValue(49.09)
+        self.wid_nuy.setValue(14.15)
+        self.wid_nux.setSingleStep(0.001)
+        self.wid_nuy.setSingleStep(0.001)
+        self.wid_nux.setDecimals(4)
+        self.wid_nuy.setDecimals(4)
 
-        acttrajfit = QAction('Open Traj. Fit', menubar)
-        Window = create_window_from_widget(SelectBPMs, title='BPM List')
-        util.connect_newprocess(acttrajfit, ['sirius-hla-si-ap-trajfit.py', ])
-        menubar.addAction(acttrajfit)
+        wid.layout().addWidget(QLabel('nux', wid), 1, 0)
+        wid.layout().addWidget(QLabel('nuy', wid), 2, 0)
+        wid.layout().addWidget(self.wid_nux, 1, 1)
+        wid.layout().addWidget(self.wid_nuy, 2, 1)
+        wid.layout().addWidget(pusb, 3, 0, 1, 2)
+        wid.layout().addWidget(self.lab_tune, 4, 0, 1, 2)
+        return wid
 
+    def get_param_control_widget(self, parent):
+        """."""
+        wid = QGroupBox('Fitting Control', parent)
+        wid.setLayout(QGridLayout())
 
-if __name__ == '__main__':
-    import sys as _sys
-    from siriushla.sirius_application import SiriusApplication
+        self.wid_nr_iter = QSpinBox(wid)
+        self.wid_tol = QLineEdit('100', wid)
+        self.wid_thres = QLineEdit('10.0', wid)
+        self.lab_fitting = QLabel(wid)
+        pusb = QPushButton('Fit Trajectory', wid)
+        pusb.clicked.connect(self._do_fitting)
 
-    app = SiriusApplication()
-    main_win = MainWindow(LL_PREF, 'SI')
-    main_win.show()
-    _sys.exit(app.exec_())
+        self.wid_nr_iter.setValue(10)
+        self.wid_tol.setValidator(QDoubleValidator())
+        self.wid_thres.setValidator(QDoubleValidator())
+
+        wid.layout().addWidget(QLabel('# Iterations', wid), 1, 0)
+        wid.layout().addWidget(QLabel('Tolerance [um]', wid), 2, 0)
+        wid.layout().addWidget(QLabel('Min BPM Sum [%]', wid), 3, 0)
+        wid.layout().addWidget(self.wid_nr_iter, 1, 1)
+        wid.layout().addWidget(self.wid_tol, 2, 1)
+        wid.layout().addWidget(self.wid_thres, 3, 1)
+        wid.layout().addWidget(pusb, 4, 0, 1, 2)
+        wid.layout().addWidget(self.lab_fitting, 5, 0, 1, 2)
+        return wid
+
+    def get_results_widget(self, parent):
+        """."""
+        wid = QGroupBox('Fitting Results', parent)
+        wid.setLayout(QGridLayout())
+
+        self.wid_x0 = QLabel('0.000', wid)
+        self.wid_xl0 = QLabel('0.000', wid)
+        self.wid_y0 = QLabel('0.000', wid)
+        self.wid_yl0 = QLabel('0.000', wid)
+        self.wid_en0 = QLabel('0.000', wid)
+        self.wid_res = QLabel('0.000', wid)
+        wid.layout().addWidget(QLabel('x0 [mm]', wid), 1, 0)
+        wid.layout().addWidget(QLabel('xl0 [mm]', wid), 2, 0)
+        wid.layout().addWidget(QLabel('y0 [mm]', wid), 3, 0)
+        wid.layout().addWidget(QLabel('yl0 [mm]', wid), 4, 0)
+        wid.layout().addWidget(QLabel('en0 [%]', wid), 5, 0)
+        wid.layout().addWidget(QLabel('residue [um]', wid), 6, 0)
+        wid.layout().addWidget(self.wid_x0, 1, 1)
+        wid.layout().addWidget(self.wid_xl0, 2, 1)
+        wid.layout().addWidget(self.wid_y0, 3, 1)
+        wid.layout().addWidget(self.wid_yl0, 4, 1)
+        wid.layout().addWidget(self.wid_en0, 5, 1)
+        wid.layout().addWidget(self.wid_res, 6, 1)
+        return wid
+
+    def _adjust_tune(self):
+        self.lab_tune.setText('Adjusting tunes...')
+
+        tunex_goal = float(self.wid_nux.value())
+        tuney_goal = float(self.wid_nuy.value())
+
+        self.tunecorr.get_tunes(self.fit_traj.model)
+        tunemat = self.tunecorr.calc_jacobian_matrix()
+        self.tunecorr.correct_parameters(
+            model=self.fit_traj.model,
+            goal_parameters=np.array([tunex_goal, tuney_goal]),
+            jacobian_matrix=tunemat)
+
+        self.lab_tune.setText('Done!')
+
+    def _do_fitting(self):
+        self.lab_fitting.setText('Getting Orbit from SOFB...')
+        trjx, trjy, trjs = self.fit_traj.get_traj_from_sofb()
+        # trjx, trjy, trjs = self.fit_traj.simulate_sofb(
+        #     -8.2e-3, 0.1e-3, y0=0.4e-3, yl0=0, delta=0.002,
+        #     errx=0.5e-3, erry=0.3e-3)
+
+        self.lab_fitting.setText('Fitting...')
+        tol = float(self.wid_tol.text()) * 1e-6
+        max_iter = self.wid_nr_iter.value()
+        self.fit_traj.params.count_rel_thres = float(self.wid_thres.text())/100
+        vecs, _, chis = self.fit_traj.do_fitting(
+            trjx, trjy, tol=tol, max_iter=max_iter, full=True)
+        x, xl, y, yl, de = vecs[-1]
+        chi = chis[-1]
+
+        self.wid_x0.setText(f'{x*1e3:.3f}')
+        self.wid_xl0.setText(f'{xl*1e3:.3f}')
+        self.wid_y0.setText(f'{y*1e3:.3f}')
+        self.wid_yl0.setText(f'{yl*1e3:.3f}')
+        self.wid_en0.setText(f'{de*1e2:.3f}')
+        self.wid_res.setText(f'{chi*1e6:.1f}')
+        self.axes_x.set_title(
+            r"x = {:.3f}mm x' = {:.3f}mrad  $\delta$ = {:.2f}%".format(
+                x*1e3, xl*1e3, de*100))
+        self.axes_y.set_title(
+            r"y = {:.3f}mm y' = {:.3f}mrad".format(y*1e3, yl*1e3))
+
+        self.lab_fitting.setText('Calculating Trajectory...')
+
+        trjx_fit, trjy_fit = self.fit_traj.calc_traj(*vecs[-1], size=trjx.size)
+        bpmpos = self.fit_traj.twiss.spos[self.fit_traj.bpm_idx]
+        bpmpos = bpmpos[:trjx.size]
+
+        self.line_measx.set_xdata(bpmpos)
+        self.line_measy.set_xdata(bpmpos)
+        self.line_meass.set_xdata(bpmpos)
+        self.line_fitx.set_xdata(bpmpos)
+        self.line_fity.set_xdata(bpmpos)
+        self.line_measx.set_ydata(trjx*1e3)
+        self.line_measy.set_ydata(trjy*1e3)
+        self.line_meass.set_ydata(trjs)
+        self.line_fitx.set_ydata(trjx_fit*1e3)
+        self.line_fity.set_ydata(trjy_fit*1e3)
+        self.axes_x.relim()
+        self.axes_y.relim()
+        self.axes_s.relim()
+        self.axes_x.autoscale_view()
+        self.axes_y.autoscale_view()
+        self.axes_s.autoscale_view()
+        self.fig.canvas.draw()
+
+        self.lab_fitting.setText('Done!')
